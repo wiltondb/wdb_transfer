855f2d6b383886cedf0551781d5a1f961bdd83d6
alex <alex@staticlibs.net>
2024-08-04 13:26:50 +0100

    use patched tiberius on gh actions

---
+++ .github/scripts/windows-build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+pushd .. || exit /b 1
+git clone --branch sspi_response_header_type https://github.com/wiltondb/tiberius.git || exit /b 1
+popd || exit /b 1
+
+rustup install 1.70.0 || exit /b 1
+cargo +1.70.0 build --release || exit /b 1
